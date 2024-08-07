4d8fbbff43c4881db950dff6a5e1fa8b56ed5204
nhan.tran <nhan.tran@asnet.com.vn>
2024-08-07 15:03:29 +0700

    Replace ViewSets instead for APIView

--- big-practice/studentManagement/accounts/urls.py
+++ big-practice/studentManagement/accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import register, login
-
+from .views import LoginView, RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path("login/", login, name="login"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("register/", RegisterView.as_view(), name="register"),
+    # Try to use sign-in and refresh token from rest_framework_simplejwt
+    path("sign-in/", TokenObtainPairView.as_view(), name="user_sign_in"),
+    path('refresh/', TokenRefreshView.as_view(),
+         name='token_refresh')
 ]
--- big-practice/studentManagement/accounts/views.py
+++ big-practice/studentManagement/accounts/views.py
@@ -1,34 +1,47 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer
+from .models import User
+from rest_framework.permissions import AllowAny
 
 
-@api_view(['POST'])
-def register(request):
-    serializer = RegisterSerializer(data=request.data)
-    if serializer.is_valid():
-        user = serializer.save()
-        return Response(UserSerializer(user).data,
-                        status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterView(generics.CreateAPIView):
+    """
+    The API View for user registration
+    """
+
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
 
 
-@api_view(['POST'])
-def login(request):
-    serializer = LoginSerializer(data=request.data)
-    if serializer.is_valid():
-        username = serializer.validated_data['username']
-        password = serializer.validated_data['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            })
-        return Response({'error': 'Invalid credentials'},
-                        status=status.HTTP_401_UNAUTHORIZED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LoginView(generics.GenericAPIView):
+    """
+    The API View for user login.
+    """
+
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        """
+        Handles the HTTP POST request for user login
+        """
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                })
+            return Response({'error': 'Invalid credentials'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
