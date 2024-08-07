5aeede6409fa735974a4915f7cb94dc87d38e5c9
nhan.tran <nhan.tran@asnet.com.vn>
2024-08-07 11:52:33 +0700

    Intergrate swagger

--- big-practice/studentManagement/studentManagement/settings.py
+++ big-practice/studentManagement/studentManagement/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'drf_yasg',
     "rest_framework",
     "student",
     "courses",
