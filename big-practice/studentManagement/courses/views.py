119b9b565c46b725ac61db861a5d2fd0fd7d67e2
nhan.tran <nhan.tran@asnet.com.vn>
2024-08-08 15:46:00 +0700

    Create API to CRUD course

--- big-practice/studentManagement/courses/views.py
+++ big-practice/studentManagement/courses/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets, permissions
 from .serializers import CourseSerializer
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Course
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -9,8 +14,8 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
-    def get_permissions(self):
-        if self.action == 'list':
-            return [permissions.AllowAny()]
+    # def get_permissions(self):
+    #     if self.action == 'list':
+    #         return [permissions.AllowAny()]
 
-        return [permissions.IsAuthenticated()]
+    #     return [permissions.IsAuthenticated()]
