f9abfe81be5ec4688efa7498cfd643bccdccb932
nhan.tran <nhan.tran@asnet.com.vn>
2024-08-08 14:39:20 +0700

    Create API CRUD student

--- big-practice/studentManagement/student/views.py
+++ big-practice/studentManagement/student/views.py
@@ -1,16 +1,19 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics, status
 from student.serializers import StudentSerializer
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Student
 
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+
+
 class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    # filter_backends = [OrderingFilter, SearchFilter]
 
-    # def get_permissions(self):
-    #     if self.action == 'list':
-    #         return [permissions.AllowAny()]
-
-    #     return [permissions.IsAuthenticated()]
+    pagination_class = CustomPagination
+    ordering_fields = ["first_name"]
+    queryset = Student.objects.filter(is_active=True)
+    serializer_class = StudentSerializer
