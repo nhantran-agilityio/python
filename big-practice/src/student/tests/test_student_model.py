2c6bb8a1e5e62678b22096a1f968c437ac774e06
Nhan Tran <nhan.tran@asnet.com.vn>
2024-10-28 18:15:16 +0700

    create dumped data

--- big-practice/src/student/tests/test_student_model.py
+++ big-practice/src/student/tests/test_student_model.py
@@ -6,7 +6,8 @@ from student.models import Student
 class StudentModelTest(TestCase):
     def setUp(self):
         today = date.today()
-
+        fixtures = ['student.json']
+        print("studentMock", fixtures)
         self.student = Student.objects.create(
             first_name='Nhan',
             last_name='Tran',
