d8201ea98c7a76753b9aa8ba51e2ba3ded9aede6
Nhan Tran <nhan.tran@asnet.com.vn>
2025-02-19 08:28:07 +0700

    Fixed bug reset password

--- big-practice/src/accounts/utils.py
+++ big-practice/src/accounts/utils.py
@@ -2,6 +2,14 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
+    """
+    A token generator class for email verification.
+
+    This class inherits from PasswordResetTokenGenerator and is used to generate
+    and validate tokens for email verification purposes. It does not add any
+    additional functionality to the base class but serves as a specific
+    implementation for email verification.
+    """
     pass
 
 
