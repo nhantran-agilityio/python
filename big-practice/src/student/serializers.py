a048114c2e83ddec73be77294ee2843edef97d70
Nhan Tran <nhan.tran@asnet.com.vn>
2025-03-06 11:30:46 +0700

    Refactor code - remove unused code

--- big-practice/src/student/serializers.py
+++ big-practice/src/student/serializers.py
@@ -53,7 +53,7 @@ class StudentSerializer(serializers.ModelSerializer):
         # Clear existing courses and add new ones
         instance.courses.clear()
         for course_data in courses_data:
-            course, created = Course.objects.get_or_create(**course_data)
+            course = Course.objects.get_or_create(**course_data)
             instance.courses.add(course)
 
         return instance
