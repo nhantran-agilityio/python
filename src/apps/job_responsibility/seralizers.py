39ff7eb0a3c02555807e659fb3ca6adbf2f56f1a
Nhan Tran <nhan.tran@asnet.com.vn>
2025-03-20 18:29:54 +0700

    [HR]- Create job_responsibility app

---
+++ src/apps/job_responsibility/seralizers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import JobResponsibility
+
+
+class JobResponsibilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobResponsibility
+        fields = '__all__'
---
+++ src/apps/job_responsibility/views.py
@@ -0,0 +1,15 @@
+
+from rest_framework import generics
+from .models import JobResponsibility
+from apps.job_responsibility.seralizers import JobResponsibilitySerializer
+
+
+class JobResponsibilityListCreateView(generics.ListCreateAPIView):
+    queryset = JobResponsibility.objects.all()
+    serializer_class = JobResponsibilitySerializer
+
+
+class JobResponsibilityDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = JobResponsibility.objects.all()
+    serializer_class = JobResponsibilitySerializer
+
