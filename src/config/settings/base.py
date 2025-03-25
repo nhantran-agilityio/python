4f7b39a3f8c0f0cf7bb80c00ac0ea90b4a3b04ed
Nhan Tran <nhan.tran@asnet.com.vn>
2025-03-25 17:48:56 +0700

    [HR] - Create API to filter leave history

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -215,6 +215,8 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 
+CSRF_TRUSTED_ORIGINS = ["https://human-resource.up.railway.app"]
+
 
 # SSL Configuration for Development
 SECURE_SSL_REDIRECT = True
