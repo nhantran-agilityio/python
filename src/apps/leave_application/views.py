1cc9ea41c4e976a5fd04e128a35836cd988fddec
Nhan Tran <nhan.tran@asnet.com.vn>
2025-03-31 11:14:37 +0700

    [HR] - Add pagination to leave history

--- src/apps/leave_application/views.py
+++ src/apps/leave_application/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg import openapi
+from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 from apps.leave_application.models import (
     LeaveApplication,
@@ -14,10 +15,50 @@ from apps.leave_application.serializers import (
 )
 
 
+class LeaveApplicationPagination(pagination.PageNumberPagination):
+    page_size = 5  # Number of records per page
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Override paginate_queryset to handle invalid page numbers.
+        """
+        try:
+            return super().paginate_queryset(queryset, request, view)
+        except pagination.NotFound:
+            # Return an empty list if the page does not exist
+            self.page = None
+            return []
+
+    def get_paginated_response(self, data):
+        """
+        Return a paginated response with an empty array if the page is invalid.
+        """
+        if self.page is None:
+            return Response({
+                'count': 0,
+                'next': None,
+                'previous': None,
+                'results': []
+            })
+        else:
+            return Response({
+                "metaData": {
+                    "limit": self.page.paginator.per_page,
+                    "page": self.page.number,
+                    "totalCount": self.page.paginator.count
+                },
+                "results": data
+            })
+        return super().get_paginated_response(data)
+
+
 class LeaveApplicationListView(generics.ListCreateAPIView):
     queryset = LeaveApplication.objects.all()
     filter_backends = [DjangoFilterBackend]
     serializer_class = LeaveApplicationSerializer
+    pagination_class = LeaveApplicationPagination
 
     @swagger_auto_schema(
         manual_parameters=[
