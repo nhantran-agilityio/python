75f0d6602421b2006c3607496e88e75ac9980a24
Nhan Tran <nhan.tran@asnet.com.vn>
2025-04-08 16:49:57 +0700

    [HR] -Update expired token time

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -227,8 +227,8 @@ SWAGGER_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
