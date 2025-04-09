cef825d43439b9e48e8d44f3aef6a0c497569033
Nhan Tran <nhan.tran@asnet.com.vn>
2025-04-09 15:52:01 +0700

    [HR] - Add method post and delete for profile

--- src/apps/family/views.py
+++ src/apps/family/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg import openapi
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
@@ -30,3 +31,24 @@ class FamilyDetailAPIView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_description="Create a new contact",
+        request_body=FamilySerializer,
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER,
+                              description="Token: Bearer <access_token>",
+                              type=openapi.TYPE_STRING),
+        ]
+    )
+    def post(self, request):
+        serializer = FamilySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        contact = get_object_or_404(Family, pk=pk, user=request.user)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- src/apps/financial/views.py
+++ src/apps/financial/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 from apps.accounts.models import User
-
-
 from .models import Financial
 from .serializers import FinancialSerializer
 
@@ -32,3 +32,24 @@ class FinancialDetailAPIView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_description="Create a new contact",
+        request_body=FinancialSerializer,
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER,
+                              description="Token: Bearer <access_token>",
+                              type=openapi.TYPE_STRING),
+        ]
+    )
+    def post(self, request):
+        serializer = FinancialSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        contact = get_object_or_404(Financial, pk=pk, user=request.user)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
