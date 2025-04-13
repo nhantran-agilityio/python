3623c50542abadb955c2896ba10e5952306ba600
Nhan Tran <nhan.tran@asnet.com.vn>
2025-04-13 23:31:41 +0700

    [HR] -Update config upload to file to S3

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -82,7 +82,15 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "ap-southeast-1")
+AWS_DEFAULT_ACL = "public-read"
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "ap-southeast-2")
+# MEDIA settings
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+# MEDIA_URL = os.getenv("AWS_S3_CUSTOM_DOMAIN", "/media/") + "/"
+
+
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 AWS_QUERYSTRING_AUTH = False
 AWS_DEFAULT_ACL = None
@@ -201,10 +209,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# MEDIA settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.User'
 
