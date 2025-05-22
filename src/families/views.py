09ee5694f63b0028e165d01472f68ceaccf8fa79
Nhan Tran <nhan.tran@asnet.com.vn>
2025-05-22 17:28:58 +0700

    [HR] -Remove check permission in get_queryset

--- src/families/views.py
+++ src/families/views.py
@@ -1,19 +1,15 @@
-from django.forms import ValidationError
 from core.api_views import BaseAuthenticatedModelViewSet
+from core.custom_permissions import IsAdminOrOwner
 from .models import Family
 from .serializers import FamilySerializer
 
 
 class FamilyViewSet(BaseAuthenticatedModelViewSet):
     serializer_class = FamilySerializer
+    permission_classes = [IsAdminOrOwner]
 
     def get_queryset(self):
-        user = self.request.user
-        if not user.is_authenticated:
-            return Family.objects.none()
-        return Family.objects.filter(user=user)
+        return Family.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        if not self.request.user.is_authenticated:
-            raise ValidationError("User must be authenticated to create a Family.")
         serializer.save(user=self.request.user)
--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,8 +1,15 @@
 from core.api_views import BaseAuthenticatedModelViewSet
 from jobs.models import Job
 from jobs.serializers import JobSerializer
+from core.custom_permissions import IsAdminOrOwner
 
 
 class JobViewSet(BaseAuthenticatedModelViewSet):
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
+    permission_classes = [IsAdminOrOwner]
+
+    def get_queryset(self):
+        return Job.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
