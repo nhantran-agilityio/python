c9aa46fb0ed57bfdadc9d0028c86d45457a6bbe6
Nhan Tran <nhan.tran@asnet.com.vn>
2025-05-22 17:20:16 +0700

    [HR] -Update more unauthorized_request in api_view

--- src/core/api_views.py
+++ src/core/api_views.py
@@ -41,6 +41,17 @@ class CommonViewSet:
         return ApiResponse(data=None, message=message,
                            status_code=status.HTTP_501_NOT_IMPLEMENTED)
 
+    def unauthorized_request(self, field=None, message=None):
+        """
+        Return unauthorized request with message content
+        """
+        # Build up the error content.
+        return ApiResponse(
+            data=None,
+            message=message,
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
 
 class AuthenticatedViewSet:
     permission_classes = [IsAuthenticated]
