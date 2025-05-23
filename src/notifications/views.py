99d645f121cf8e3276e20b0d3124741db2be03b3
Nhan Tran <nhan.tran@asnet.com.vn>
2025-05-23 16:06:09 +0700

    [HR] -Add finalize_response to api view ModelViewSet

--- src/notifications/views.py
+++ src/notifications/views.py
@@ -1,13 +1,10 @@
 from core.api_views import BaseModelViewSet
 from core.custom_permissions import IsAdminOrOwner
-from core.mixins import CustomRetrieveModelMixin, CustomUpdateModelMixin
 from .models import Notification
 from .serializers import NotificationSerializer
 
 
 class NotificationsViewSet(
-    CustomRetrieveModelMixin,
-    CustomUpdateModelMixin,
     BaseModelViewSet,
 ):
     serializer_class = NotificationSerializer
